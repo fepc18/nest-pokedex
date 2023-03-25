5b3598d91388067e9f7a34c5de1344c09edb8d26
fepc18 <fepc18@gmail.com>
2023-03-24 20:32:36 -0500

    seed controller

---
+++ src/seed/seed.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { SeedService } from './seed.service';
+
+
+@Controller({
+  version: '1',
+  path:'seed'
+})
+export class SeedController {
+  constructor(private readonly seedService: SeedService) {}
+
+
+  @Get()
+  findAll() {
+    return this.seedService.executeSeed();
+  }
+
+}
